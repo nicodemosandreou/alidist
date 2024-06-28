96aa13fb61ab60bd4461509ca82df0db6c008b15
Nicodemos Andreou <nicodemos.andreou@cern.ch>
2024-06-28 10:24:30 +0100

    update

--- onnxruntime.sh
+++ onnxruntime.sh
@@ -6,7 +6,7 @@ requires:
   - protobuf
   - re2
   - boost
- 
+
 build_requires:
   - CMake
   - alibuild-recipe-tools
@@ -19,8 +19,14 @@ prepend_path:
 
 mkdir -p $INSTALLROOT
 
-# Export ROCm path
-export ROCM_PATH=/opt/rocm  # Change this to your actual ROCm installation path
+# Set up environment variables for ROCm
+export ROCM_PATH=/opt/rocm  
+export ROCM_HOME=$ROCM_PATH
+export PATH=$ROCM_HOME/bin:$PATH
+export PATH=$ROCM_HOME/llvm/bin:$PATH
+export LD_LIBRARY_PATH=$ROCM_HOME/lib:$ROCM_HOME/lib64:$LD_LIBRARY_PATH
+export HIP_PLATFORM=hcc
+export GPU_TARGETS=gfx906
 
 cmake "$SOURCEDIR/cmake"                                                              \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                                             \
@@ -32,7 +38,7 @@ cmake "$SOURCEDIR/cmake"                                                        
       -Donnxruntime_BUILD_SHARED_LIB=ON                                               \
       -DProtobuf_USE_STATIC_LIBS=ON                                                   \
       -Donnxruntime_USE_ROCM=ON                                                       \
-      -Donnxruntime_ROCM_HOME=$ROCM_PATH                                              \
+      -Donnxruntime_ROCM_HOME=$ROCM_HOME                                              \
       ${PROTOBUF_ROOT:+-DProtobuf_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.a}           \
       ${PROTOBUF_ROOT:+-DProtobuf_LITE_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf-lite.a} \
       ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_LIBRARY=$PROTOBUF_ROOT/lib/libprotoc.a}      \
@@ -41,7 +47,9 @@ cmake "$SOURCEDIR/cmake"                                                        
       ${RE2_ROOT:+-DRE2_INCLUDE_DIR=${RE2_ROOT}/include}                              \
       ${BOOST_ROOT:+-DBOOST_INCLUDE_DIR=${BOOST_ROOT}/include}                        \
       -DCMAKE_CXX_FLAGS="$CXXFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-error=unused-but-set-variable" \
-      -DCMAKE_C_FLAGS="$CFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-error=unused-but-set-variable"
+      -DCMAKE_C_FLAGS="$CFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-error=unused-but-set-variable"      \
+      -DCMAKE_HIP_COMPILER=$ROCM_HOME/llvm/bin/clang++                                \
+      -D__HIP_PLATFORM_AMD__=1
 
 cmake --build . -- ${JOBS:+-j$JOBS} install
 
@@ -55,3 +63,4 @@ cat >> "$MODULEFILE" <<EoF
 set ${PKGNAME}_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path ROOT_INCLUDE_PATH \$${PKGNAME}_ROOT/include/onnxruntime
 EoF
+
