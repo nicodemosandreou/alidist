c604dbda0242adaa32dec13d6f041498ce756e3d
Nicodemos Andreou <nicodemos.andreou@cern.ch>
2024-07-03 14:15:37 +0100

    update

--- onnxruntime.sh
+++ onnxruntime.sh
@@ -29,7 +29,6 @@ export HIP_PLATFORM=hcc
 export HCC_AMDGPU_TARGET=gfx906 
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 
-
 # Save current LD_LIBRARY_PATH
 OLD_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 
@@ -46,7 +45,7 @@ cmake "$SOURCEDIR/cmake"                                                        
       -Donnxruntime_BUILD_SHARED_LIB=ON                                               \
       -DProtobuf_USE_STATIC_LIBS=ON                                                   \
       -Donnxruntime_USE_ROCM=ON                                                       \
-      -Donnxruntime_ROCM_HOME=$ROCM_HOME                                               \
+      -Donnxruntime_ROCM_HOME=$ROCM_HOME                                              \
       ${PROTOBUF_ROOT:+-DProtobuf_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.a}           \
       ${PROTOBUF_ROOT:+-DProtobuf_LITE_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf-lite.a} \
       ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_LIBRARY=$PROTOBUF_ROOT/lib/libprotoc.a}      \
@@ -54,7 +53,7 @@ cmake "$SOURCEDIR/cmake"                                                        
       ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_EXECUTABLE=$PROTOBUF_ROOT/bin/protoc}        \
       ${RE2_ROOT:+-DRE2_INCLUDE_DIR=${RE2_ROOT}/include}                              \
       ${BOOST_ROOT:+-DBOOST_INCLUDE_DIR=${BOOST_ROOT}/include}                        \
-      -DCMAKE_CXX_FLAGS="$CXXFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-error=unused-but-set-variable" \
+      -DCMAKE_CXX_FLAGS="$CXXFLAGS -std=c++11 -Wno-unknown-warning -Wno-unknown-warning-option -Wno-error=unused-but-set-variable" \
       -DCMAKE_C_FLAGS="$CFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-error=unused-but-set-variable"      \
       -DCMAKE_HIP_COMPILER=$ROCM_HOME/llvm/bin/clang++                                \
       -D__HIP_PLATFORM_AMD__=1
