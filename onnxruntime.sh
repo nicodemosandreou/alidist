d3e6cd5f0a1709721c09aa60de3a001a0f2a9f99
nicodemosandreou <131385658+nicodemosandreou@users.noreply.github.com>
2024-08-27 10:54:28 +0100

    MI100

--- onnxruntime.sh
+++ onnxruntime.sh
@@ -1,12 +1,12 @@
 package: ONNXRuntime
 version: "%(tag_basename)s"
-tag: v1.16.3
+tag: v1.18.0
 source: https://github.com/microsoft/onnxruntime
 requires:
   - protobuf
   - re2
   - boost
-
+  - abseil
 build_requires:
   - CMake
   - alibuild-recipe-tools
@@ -19,22 +19,6 @@ prepend_path:
 
 mkdir -p $INSTALLROOT
 
-# Set up environment variables for ROCm
-export ROCM_PATH=/opt/rocm  
-export ROCM_HOME=$ROCM_PATH
-export PATH=$ROCM_HOME/bin:$PATH
-export PATH=$ROCM_HOME/llvm/bin:$PATH
-export LD_LIBRARY_PATH=$ROCM_HOME/lib:$ROCM_HOME/lib64:$LD_LIBRARY_PATH
-export HIP_PLATFORM=hcc
-export HCC_AMDGPU_TARGET=gfx906 
-export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
-
-# Save current LD_LIBRARY_PATH
-OLD_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
-
-# Temporarily remove ROCm's libstdc++.so.6 from LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | tr ':' '\n' | grep -v "$ROCM_HOME/lib" | tr '\n' ':')
-
 cmake "$SOURCEDIR/cmake"                                                              \
       -DCMAKE_INSTALL_PREFIX=$INSTALLROOT                                             \
       -DCMAKE_BUILD_TYPE=Release                                                      \
@@ -43,9 +27,18 @@ cmake "$SOURCEDIR/cmake"                                                        
       -Donnxruntime_BUILD_UNIT_TESTS=OFF                                              \
       -Donnxruntime_PREFER_SYSTEM_LIB=ON                                              \
       -Donnxruntime_BUILD_SHARED_LIB=ON                                               \
-      -DProtobuf_USE_STATIC_LIBS=ON                                                   \
       -Donnxruntime_USE_ROCM=ON                                                       \
-      -Donnxruntime_ROCM_HOME=$ROCM_HOME                                              \
+      -Donnxruntime_ROCM_HOME=/opt/rocm                                               \
+      -DCMAKE_HIP_COMPILER=/opt/rocm/llvm/bin/clang++                                 \
+      -D__HIP_PLATFORM_AMD__=1                                                        \
+      -DCMAKE_HIP_ARCHITECTURES=gfx906,gfx908                                         \
+      -Donnxruntime_USE_COMPOSABLE_KERNEL=OFF                                         \
+      -Donnxruntime_USE_ROCBLAS_EXTENSION_API=ON                                      \
+      -Donnxruntime_USE_COMPOSABLE_KERNEL_CK_TILE=ON                                  \
+      -Donnxruntime_ENABLE_TRAINING=OFF                                               \
+      -DProtobuf_USE_STATIC_LIBS=ON                                                   \
+      -Donnxruntime_DISABLE_RTTI=OFF                                                  \
+      -DMSVC=OFF                                                                      \
       ${PROTOBUF_ROOT:+-DProtobuf_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf.a}           \
       ${PROTOBUF_ROOT:+-DProtobuf_LITE_LIBRARY=$PROTOBUF_ROOT/lib/libprotobuf-lite.a} \
       ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_LIBRARY=$PROTOBUF_ROOT/lib/libprotoc.a}      \
@@ -53,15 +46,10 @@ cmake "$SOURCEDIR/cmake"                                                        
       ${PROTOBUF_ROOT:+-DProtobuf_PROTOC_EXECUTABLE=$PROTOBUF_ROOT/bin/protoc}        \
       ${RE2_ROOT:+-DRE2_INCLUDE_DIR=${RE2_ROOT}/include}                              \
       ${BOOST_ROOT:+-DBOOST_INCLUDE_DIR=${BOOST_ROOT}/include}                        \
-      -DCMAKE_CXX_FLAGS="$CXXFLAGS -std=c++11 -Wno-unknown-warning -Wno-unknown-warning-option -Wno-error=unused-but-set-variable" \
-      -DCMAKE_C_FLAGS="$CFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-error=unused-but-set-variable"      \
-      -DCMAKE_HIP_COMPILER=$ROCM_HOME/llvm/bin/clang++                                \
-      -D__HIP_PLATFORM_AMD__=1
+      -DCMAKE_CXX_FLAGS="$CXXFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-pass-failed -Wno-error=unused-but-set-variable -Wno-pass-failed=transform-warning" \
+      -DCMAKE_C_FLAGS="$CFLAGS -Wno-unknown-warning -Wno-unknown-warning-option -Wno-pass-failed -Wno-error=unused-but-set-variable -Wno-pass-failed=transform-warning"
 
 cmake --build . -- ${JOBS:+-j$JOBS} install
-
-# Restore the original LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$OLD_LD_LIBRARY_PATH
 
 # Modulefile
 mkdir -p "$INSTALLROOT/etc/modulefiles"
@@ -72,5 +60,4 @@ cat >> "$MODULEFILE" <<EoF
 # Our environment
 set ${PKGNAME}_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path ROOT_INCLUDE_PATH \$${PKGNAME}_ROOT/include/onnxruntime
-prepend-path LD_LIBRARY_PATH \$${PKGNAME}_ROOT/lib
 EoF
